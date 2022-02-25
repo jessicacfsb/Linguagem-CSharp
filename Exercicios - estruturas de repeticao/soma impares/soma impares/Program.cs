10e1be0aa7099341efc90d397b47854b328995a6
jessica.cfsb <jessica.cfsb@gmail.com>
2022-02-25 15:07:31 -0300

    Exercicios iniciais - C#

---
+++ Exercicios - estruturas de repeticao/soma impares/soma impares/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace soma_impares {
+    class Program {
+        static void Main(string[] args) {
+
+            int num1, num2, troca, soma;        
+
+            Console.WriteLine("Digite dois numeros: ");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2) {
+                troca = num1;
+                num1 = num2;
+                num2 = troca;
+            }
+
+            soma = 0;
+            for (int i = num1+1; i < num2; i++) {
+                if (i % 2 != 0) {
+                    soma = soma + i;
+                }
+            }
+
+            Console.WriteLine("SOMA DOS IMPARES = " + soma);
+        }
+    }
+}
