145d36909c0a8bd8a6dd65d534d3844bbb85e323
jessica.cfsb <jessica.cfsb@gmail.com>
2022-02-25 10:59:02 -0300

    first commit

---
+++ entrada de dados/entrada de dados/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace entrada_de_dados {
+    class Program {
+        static void Main(string[] args) {
+
+            int x, y;
+            double x1;
+
+            x = 10;
+            y = 20;
+            x1 = 2.0;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(x1.ToString("F2" , CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ ola, mundo/ola, mundo/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ola__mundo {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, world!");
+            Console.Write("Bom dia!");
+            Console.WriteLine("Boa tarde!");
+        }
+    }
+}
