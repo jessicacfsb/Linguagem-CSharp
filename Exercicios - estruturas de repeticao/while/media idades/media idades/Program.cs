e89991bb556a251c16124fecdbfffd8b6b734e56
jessica.cfsb <jessica.cfsb@gmail.com>
2022-03-01 16:48:27 -0300

    Exercicios - c#

---
+++ Exercicios - estruturas de repeticao/while/media idades/media idades/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace media_idades {
+    class Program {
+        static void Main(string[] args) {
+
+            int idades, soma, cont;
+            double media;
+
+            Console.WriteLine("Digite as idades: ");
+            idades = int.Parse(Console.ReadLine());
+
+            if (idades < 0) {
+                Console.WriteLine("IMPOSSIVEL CALCULAR");
+            }
+            else {
+                soma = 0;
+                cont = 0;
+                while (idades > 0) {
+                    soma = soma + idades;
+                    cont++;
+                    idades = int.Parse(Console.ReadLine());
+                }
+
+                media = (double)soma / cont;
+                Console.WriteLine("MEDIA = " + media.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
---
+++ Exercicios - estruturas de repeticao/while/senha fixa/senha fixa/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace senha_fixa {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
